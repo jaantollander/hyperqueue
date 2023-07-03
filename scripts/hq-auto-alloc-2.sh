ee58b066d225645929c51693c0c1ddb44ddda1f5
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-07-03 15:21:25 +0300

    test autoallocator with multiple workers

---
+++ scripts/hq-auto-alloc-2.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#SBATCH --partition=interactive
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2000
+#SBATCH --time=00:15:00
+
+#module load hyperqueue openbabel
+module load openbabel
+
+# Use local hyperqueue
+export PATH="$PWD/bin:$PATH"
+
+# Specify a location for the HyperQueue server
+export HQ_SERVER_DIR=${PWD}/hq-server/${SLURM_JOB_ID}
+mkdir -p "${HQ_SERVER_DIR}"
+
+# Start the server in the background (&) and wait until it has started
+hq server start &
+until hq job list &>/dev/null ; do sleep 1 ; done
+
+# Start the workers (one per node, in the background) and wait until they have started
+(
+    unset -v $(printenv | grep --only-matching '^SLURM_[[:upper:]_]*') &&
+    hq alloc add slurm \
+        --time-limit 10m \
+        --workers-per-alloc 2 \
+        --cpus 20 \
+        --backlog 1 \
+        --max-worker-count 1 \
+        -- \
+        --cpus-per-task 20 \
+        --mem-per-cpu 1500 \
+        --partition large &
+)
+
+# Submit jobs
+for i in {1..1000} ; do
+    hq submit --stdout=none --stderr=none --cpus=1 bash -c "echo $i; sleep 1"
+done
+hq job wait all
+
+# Shut down the HyperQueue workers and server
+hq worker stop all
+hq server stop
