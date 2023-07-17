57f84eabe55a9ea787e9765f143a347c0e94a811
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-07-17 14:07:18 +0300

    fix

--- hyperqueue/localdisk/task/archive.sh
+++ hyperqueue/localdisk/task/archive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-FILENAME="$LOCAL_SCRATCH/$SLURMD_NODENAME.sdf.tar.gz"
-tar -czf "$FILENAME" -- "$LOCAL_SCRATCH/smiles/"*.sdf
-cp "$FILENAME" "$PWD"
+FILENAME="$SLURMD_NODENAME.sdf.tar.gz"
+(cd "$LOCAL_SCRATCH" && tar -czf "$FILENAME" -- ./smiles/*.sdf)
+cp "$LOCAL_SCRATCH/$FILENAME" "$PWD"
