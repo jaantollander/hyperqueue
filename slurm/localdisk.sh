aaa9a198edbeacc2668e5b41c465f418e520964a
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-07-17 09:34:31 +0300

    comments

--- slurm/localdisk.sh
+++ slurm/localdisk.sh
@@ -7,5 +7,10 @@
 #SBATCH --mem-per-cpu=1000
 #SBATCH --gres=nvme:1
 
+# Run one slurm task on each allocated node.
+# Writes the hostname to a file on the local disk.
 srun -m arbitrary -w "$SLURM_JOB_NODELIST" bash -c "hostname > \$LOCAL_SCRATCH/hostname"
+
+# Run one slurm task on each allocated slurm task.
+# Prints the contents of the file on the local disk.
 srun bash -c "cat \$LOCAL_SCRATCH/hostname"
