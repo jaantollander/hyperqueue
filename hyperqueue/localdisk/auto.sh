287a2af219743fae19977a4704b8bdc7639e94a4
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2023-07-17 10:51:21 +0300

    refactoring

--- hyperqueue/localdisk/auto.sh
+++ hyperqueue/localdisk/auto.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#SBATCH --output=%j.out
 #SBATCH --partition=interactive
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -6,7 +7,8 @@
 #SBATCH --mem-per-cpu=2000
 #SBATCH --time=00:15:00
 
-module load hyperqueue openbabel
+module load hyperqueue/0.16.0
+module load openbabel
 
 # Specify a location for the HyperQueue server
 export HQ_SERVER_DIR=${PWD}/.hq-server/${SLURM_JOB_ID}
@@ -16,6 +18,7 @@ mkdir -p "${HQ_SERVER_DIR}"
 hq server start &
 until hq job list &>/dev/null ; do sleep 1 ; done
 
+# TODO: add start and stop scripts for extract and archive
 # Start the workers in the background
 (
     unset -v $(printenv | grep --only-matching '^SLURM_[[:upper:]_]*') &&
@@ -39,12 +42,12 @@ hq job wait all
 # Submit each Open Babel conversion as a separate HyperQueue job
 FILES=$(tar -tf ./data/smiles.tar.gz | grep "\.smi")
 for FILE in $FILES ; do
-    hq submit --stdout=none --stderr=none --cpus=1 ./hyperqueue/localdisk/task/gen3d.sh "$FILE" &
+    hq submit --stdout=none --stderr=none --cpus=1 ./hyperqueue/localdisk/task/gen3d.sh "$FILE"
 done
 hq job wait all
 
 # Compress the output .sdf files and copy the package back to /scratch
-hq submit --stdout=none --stderr=none --cpus=all ./hyperqueue/localdisk/task/archive-copy.sh "$SLURM_SUBMIT_DIR" &
+hq submit --stdout=none --stderr=none --cpus=all ./hyperqueue/localdisk/task/archive.sh "$SLURM_SUBMIT_DIR" &
 hq job wait all
 
 # Shut down the HyperQueue workers and server
---
+++ hyperqueue/localdisk/manual.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH --output=%j.out
+#SBATCH --partition=large
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=20
+#SBATCH --mem-per-cpu=1000
+#SBATCH --time=00:15:00
+#SBATCH --gres=nvme:1
+
+module load hyperqueue/0.16.0
+module load openbabel
+
+export HQ_SERVER_DIR=$PWD/.hq-server/$SLURM_JOB_ID
+mkdir -p "$HQ_SERVER_DIR"
+
+hq server start &
+
+until hq job list &>/dev/null ; do sleep 1 ; done
+
+srun --overlap --cpu-bind=none --mpi=none hq worker start \
+    --manager slurm \
+    --idle-timeout 5m \
+    --on-server-lost finish-running \
+    --cpus="$SLURM_CPUS_PER_TASK" \
+    --resource "mem=sum($((SLURM_CPUS_PER_TASK * SLURM_MEM_PER_CPU * 1000000)))" &
+
+hq worker wait "$SLURM_NTASKS"
+
+srun -m arbitrary -w "$SLURM_JOB_NODELIST" ./task/extract.sh
+
+# TODO: try --each-line <(echo "$FILES"), HQ_ENTRY
+FILES=$(tar -tf ./data/smiles.tar.gz | grep "\.smi")
+for FILE in $FILES ; do
+    hq submit --stdout=none --stderr=none --cpus=1 ./task/gen3d.sh "$FILE"
+done
+
+hq job wait all
+
+srun -m arbitrary -w "$SLURM_JOB_NODELIST" ./task/archive.sh "$SLURM_SUBMIT_DIR"
+
+hq worker stop all
+hq server stop
